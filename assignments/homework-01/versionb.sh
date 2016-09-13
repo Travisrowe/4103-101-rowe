580e4e91041126410b74db7348daef1485b7b94f
Travisrowe <travisrowe18@gmail.com>
2016-09-13 02:19:28 -0500

    Update versionb.sh

--- assignments/homework-01/versionb.sh
+++ assignments/homework-01/versionb.sh
@@ -6,11 +6,11 @@
 
 DATE=$(date +%Y-%m-%d) #Same date format as versiona.sh
 copiedFile=$DATE'_'$1 #Create String to work with
-echo $copiedFile
 fileType="${copiedFile%.*}" #Substring of the file type (e.g. .txt)
 baseOfFile="basename $1 fileType" #base name of the file
 
-
+cp $copiedFile $PWD/$baseOfFile'_'$DATE$fileType #replace file that versiona.sh made
+                                                 #with file that has date at the end
 
 
 
