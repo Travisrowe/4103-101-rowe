fe41d1fd68ad7090139314784d35470866d5ad98
Travisrowe <travisrowe18@gmail.com>
2016-09-13 01:37:56 -0500

    Update versiona.sh

--- assignments/homework-01/versiona.sh
+++ assignments/homework-01/versiona.sh
@@ -2,8 +2,8 @@
 # Copies a file with the current date added to the beginning
 # of the file name
 
-DATE=$(date +%Y-%m-%d)
+DATE=$(date +%Y-%m-%d_)
 echo $DATE
 
-cp $1 $PWD/"$DATE+_+$1"
+cp $1 $PWD/"$DATE$1"
 ls -l
