57eebb7743988c134da4b42b4e0a320b65a970f0
Travisrowe <travisrowe18@gmail.com>
2016-09-12 23:31:45 -0500

    Update sum_args.sh

--- assignments/homework-01/sum_args.sh
+++ assignments/homework-01/sum_args.sh
@@ -3,4 +3,4 @@
 read var1 var2 var3 var4 var5 var6 var7 var8 var9
 
 let "a = var1 + var2 + var3 + var4 + var5 + var6 + var7 + var8 + var9"
-echo "var1+var2+var3+var4+var5+var6+var7+var8+var9=$a"
+echo "$var1+$var2+$var3+$var4+$var5+$var6+$var7+$var8+$var9=$a"
