97e60ef5a0a73657d9e602acfeb8b5881ac39461
Travisrowe <travisrowe18@gmail.com>
2016-09-13 03:22:00 -0500

    Update myrandom.sh

--- assignments/homework-01/myrandom.sh
+++ assignments/homework-01/myrandom.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #Prints a random word
 
-let a=$RANDOM%250000
+#numWords="wc -l /usr/share/dict/words"
+#let a=$RANDOM%$numWords+1 #a is a random number between 1 and the number of
+                          #words in the dictionary
+n=$(cat /usr/share/dict/words | wc -l)
+#cat -n /usr/share/dict/words | grep -w $(jot -r 1 1 $n) | cut -f2
 
 echo "Random Word: "
-cat /usr/share/dict/words | cut -d$' ' -f $a
+cat -n /usr/share/dict/words | grep -w $(jot -r 1 1 $n) | cut -f2
+#cat /usr/share/dict/words | cut -d$' ' -f $a
