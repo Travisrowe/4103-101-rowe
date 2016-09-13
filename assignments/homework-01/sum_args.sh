6554fcf4b68068a56c331883086f907812be7dde
Travisrowe <travisrowe18@gmail.com>
2016-09-12 23:40:50 -0500

    Update sum_args.sh

--- assignments/homework-01/sum_args.sh
+++ assignments/homework-01/sum_args.sh
@@ -2,5 +2,5 @@
 # demonstrate variable scope 1.
 #read var1 var2 var3 var4 var5 var6 var7 var8 var9
 
-let "a = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9"
+let "a = $1 + $2 + $3 + $4 + $5 + $6 + $7 + $8 + $9"
 echo "$1+$2+$3+$4+$5+$6+$7+$8+$9=$a"
