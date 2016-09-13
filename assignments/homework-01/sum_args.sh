ec6a84448f801a9dd7c4557f457c0c74436a28af
Travisrowe <travisrowe18@gmail.com>
2016-09-13 00:52:33 -0500

    Added if statements

--- assignments/homework-01/sum_args.sh
+++ assignments/homework-01/sum_args.sh
@@ -2,6 +2,52 @@
 # demonstrate variable scope 1.
 #read var1 var2 var3 var4 var5 var6 var7 var8 var9
 
-echo $#
+if [$# eq 1]
+then
+let a=$1
+echo $1=$a
+fi
+
+if [$# eq 2]
+then
+let a=$1+$2
+echo $1+$2=$a
+fi
+
+if [$# eq 9]
+then
 let a=$1+$2+$3+$4+$5+$6+$7+$8+$9
 echo $1+$2+$3+$4+$5+$6+$7+$8+$9=$a
+fi
+if [$# eq 9]
+then
+let a=$1+$2+$3+$4+$5+$6+$7+$8+$9
+echo $1+$2+$3+$4+$5+$6+$7+$8+$9=$a
+fi
+if [$# eq 9]
+then
+let a=$1+$2+$3+$4+$5+$6+$7+$8+$9
+echo $1+$2+$3+$4+$5+$6+$7+$8+$9=$a
+fi
+
+if [$# eq 9]
+then
+let a=$1+$2+$3+$4+$5+$6+$7+$8+$9
+echo $1+$2+$3+$4+$5+$6+$7+$8+$9=$a
+fi
+if [$# eq 9]
+then
+let a=$1+$2+$3+$4+$5+$6+$7+$8+$9
+echo $1+$2+$3+$4+$5+$6+$7+$8+$9=$a
+fi
+if [$# eq 9]
+then
+let a=$1+$2+$3+$4+$5+$6+$7+$8+$9
+echo $1+$2+$3+$4+$5+$6+$7+$8+$9=$a
+fi
+
+if [$# eq 9]
+then
+let a=$1+$2+$3+$4+$5+$6+$7+$8+$9
+echo $1+$2+$3+$4+$5+$6+$7+$8+$9=$a
+fi
