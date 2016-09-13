df62d5de40052856863896ddba532a4e6c1cc80c
Travisrowe <travisrowe18@gmail.com>
2016-09-13 03:25:57 -0500

    Update versiona.sh

--- assignments/homework-01/versiona.sh
+++ assignments/homework-01/versiona.sh
@@ -2,6 +2,7 @@
 # Copies a file with the current date added to the beginning
 # of the file name
 
-DATE=$(date +%Y-%m-%d)
+DATE=$(date +%Y-%m-%d) #Changes format of $DATE
 
-cp $1 $PWD/$DATE'_'$1
+cp $1 $PWD/$DATE'_'$1 #Copies the file ($1) to the current directory
+                      #with the date added to the beggining of its title
