38c9307f0bfb34ef7fddec79b07b9fc995689f6c
Travisrowe <travisrowe18@gmail.com>
2016-09-13 00:57:39 -0500

    Update sum_args.sh

--- assignments/homework-01/sum_args.sh
+++ assignments/homework-01/sum_args.sh
@@ -8,49 +8,49 @@ then
   echo $1=$a
 fi
 
-if [$# -eq 2]
+if [ $# -eq 2 ]
 then
   let a=$1+$2
   echo $1+$2=$a
 fi
 
-if [$# -eq 3]
+if [ $# -eq 3 ]
 then
   let a=$1+$2+$3
   echo $1+$2+$3=$a
 fi
 
-if [$# -eq 4]
+if [ $# -eq 4 ]
 then
   let a=$1+$2+$3+$4
   echo $1+$2+$3+$4=$a
 fi
 
-if [$# -eq 5]
+if [ $# -eq 5 ]
 then
   let a=$1+$2+$3+$4+$5
   echo $1+$2+$3+$4+$5=$a
 fi
 
-if [$# -eq 6]
+if [ $# -eq 6 ]
 then
   let a=$1+$2+$3+$4+$5+$6
   echo $1+$2+$3+$4+$5+$6=$a
 fi
 
-if [$# -eq 7]
+if [ $# -eq 7 ]
 then
   let a=$1+$2+$3+$4+$5+$6+$7
   echo $1+$2+$3+$4+$5+$6+$7=$a
 fi
 
-if [$# -eq 8]
+if [ $# -eq 8 ]
 then
   let a=$1+$2+$3+$4+$5+$6+$7+$8
   echo $1+$2+$3+$4+$5+$6+$7+$8=$a
 fi
 
-if [$# -eq 9]
+if [ $# -eq 9 ]
 then
   let a=$1+$2+$3+$4+$5+$6+$7+$8+$9
   echo $1+$2+$3+$4+$5+$6+$7+$8+$9=$a
